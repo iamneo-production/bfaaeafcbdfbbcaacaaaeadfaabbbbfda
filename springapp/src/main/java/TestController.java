96adb0a50291c580a69b3fd36ba817480e3267d7
Iamneo <techadmin@iamneo.ai>
2023-10-06 10:08:54 +0000

    Backup Commit

--- springapp/src/main/java/TestController.java
+++ springapp/src/main/java/TestController.java
@@ -1,9 +1,9 @@
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class TestController{
-    @GetMapping("/hello")
+    @RequestMapping("/hello")
     public String hello(){
         return "Hello";
 
